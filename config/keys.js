804bd05e8b78f4e565acf209a7070d45ddff9ebc
MPGH5574 <gulanesgene@yahoo.com>
2020-04-08 17:41:16 +0800

    make it better2

---
+++ config/keys.js
@@ -0,0 +1,37 @@
+module.exports = {
+    google:{
+        clientID: '861009847969-9812d5rcrga52v0o23libtm32mrtkt39.apps.googleusercontent.com',
+        clientSecret: 'dUYfWB6JzCTNB9IP79sIJwgN'
+    },
+    postgresdb:{
+        user: 'xqbmngwdiptlwk',
+        host: 'ec2-3-229-210-93.compute-1.amazonaws.com',
+        database: 'dojf1av5hi1m4',
+        password: '038dda94b70871de19a09f48ac27741aff5e83e1b0590b3d369f1bbe0e39e266',
+        port:'5432  ',
+        ssl:true,
+    },
+    facebook:{
+        clientID: "3199011836810405",
+        clientSecret: "dbd0b24c3f6b2077de687718d641d302"
+    },
+    spotify:{
+        clientID:"f1d79bc9b04247a8ac345ed270009589",
+        clientSecret:"d4c38b2ce8f4405fb7ea649ba0f63290"
+    },
+
+    steam:{
+        clientID:"5273DA83FD3722289504AC44A40D65D8",
+
+    },
+    twitch:{
+        clientID:"066fg86lrz2urndbfftczazblkjsf4",
+        clientSecret:"ze450s9mr564hzr102m83fw7d1wrsk"
+    },
+
+
+
+    session:{
+        cookieKey: 'thisismyuniquecookiekey'
+    }
+};
